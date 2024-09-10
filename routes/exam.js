c44ff7ca16457b4566c3bfb7c0a1f6d53463935b
Charly <carlosgarita3110@gmail.com>
2024-09-09 19:35:42 -0600

    refactor: improve logging in exam routes module

--- routes/exam.js
+++ routes/exam.js
@@ -5,6 +5,8 @@ const router = express.Router()
 
 router.post('/create-question', (req, res) => {
   const data = req.body
+  console.log('POST /create-question - Data recibida:', data)
+
   if (data === undefined) {
     res.status(400).json({ error: 'No se recibieron datos' })
     return
@@ -14,11 +16,14 @@ router.post('/create-question', (req, res) => {
 })
 
 router.get('/get-all', (req, res) => {
+  console.log('GET /get-all - Enviando todas las preguntas')
   res.json(questionsDummyData)
 })
 
 router.put('/update', (req, res) => {
   const data = req.body
+  console.log('PUT /update - Data recibida:', data)
+
   if (data === undefined) {
     res.status(400).json({ error: 'No se recibieron datos' })
     return
@@ -36,6 +41,8 @@ router.put('/update', (req, res) => {
 
 router.delete('/delete', (req, res) => {
   const code = req.query.code
+  console.log('DELETE /delete - Código recibido:', code)
+
   if (code === undefined) {
     res.status(400).json({ error: 'No se recibió el código' })
     return

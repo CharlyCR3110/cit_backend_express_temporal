f6a976523014a5e3cd072b8ea63b1a10e1fd999f
Charly <carlosgarita3110@gmail.com>
2024-09-11 16:16:44 -0600

    refactor: add images to questionsDummyData.js

--- routes/questionsDummyData.js
+++ routes/questionsDummyData.js
@@ -3,70 +3,80 @@ const questionsDummyData = [{
   examType: 'academic',
   question: 'Cuál es la capital de Francia?',
   options: ['Madrid', 'París', 'Berlín', 'Lisboa'],
-  correctOption: '1'
+  correctOption: '1',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '124',
   examType: 'dai',
   question: 'Cuál es el resultado de 2 + 2?',
   options: ['', '', '', ''],
-  correctOption: ''
+  correctOption: '',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '125',
   examType: 'academic',
   question: 'Cuál es el elemento químico con el símbolo O?',
   options: ['Oro', 'Oxígeno', 'Osmio', 'Ogano'],
-  correctOption: '1'
+  correctOption: '1',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '126',
   examType: 'dai',
   question: 'Define el término "algoritmo".',
   options: ['', '', '', ''],
-  correctOption: ''
+  correctOption: '',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '127',
   examType: 'academic',
   question: 'Quién escribió "Cien años de soledad"?',
   options: ['Gabriel García Márquez', 'Mario Vargas Llosa', 'Pablo Neruda', 'Julio Cortázar'],
-  correctOption: '0'
+  correctOption: '0',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '128',
   examType: 'dai',
   question: 'Qué es una función en matemáticas?',
   options: ['', '', '', ''],
-  correctOption: ''
+  correctOption: '',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '129',
   examType: 'academic',
   question: 'Cuál es el planeta más cercano al sol?',
   options: ['Venus', 'Marte', 'Mercurio', 'Tierra'],
-  correctOption: '2'
+  correctOption: '2',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '130',
   examType: 'dai',
   question: 'Cuál es la fórmula química del agua?',
   options: ['', '', '', ''],
-  correctOption: ''
+  correctOption: '',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '131',
   examType: 'academic',
   question: 'Quién pintó "La última cena"?',
   options: ['Vincent van Gogh', 'Pablo Picasso', 'Leonardo da Vinci', 'Claude Monet'],
-  correctOption: '2'
+  correctOption: '2',
+  images: ['../uploads/1.jpg']
 },
 {
   code: '132',
   examType: 'dai',
   question: 'Qué es una variable en programación?',
   options: ['', '', '', ''],
-  correctOption: ''
+  correctOption: '',
+  images: ['../uploads/1.jpg']
 }]
 
 module.exports = questionsDummyData
